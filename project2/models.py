764b7da63705a5beb4fd651b450146bbb0970e05
makki-villaluz <john_matthew88@outlook.ph>
2021-06-09 16:20:02 +0800

    added route_id and stops_id to the GPXVehicle table

--- project2/models.py
+++ project2/models.py
@@ -6,6 +6,8 @@ class GPXVehicle(db.Model):
     filename = db.Column(db.String(60), unique=True, nullable=False)
     name = db.Column(db.String(60), unique=True, nullable=False)
     date_uploaded = db.Column(db.Date, default=datetime.date.today(), nullable=False)
+    route_id = db.Column(db.Integer, nullable=False)
+    stops_id = db.Column(db.Integer, nullable=False)
 
 class GPXRoute(db.Model):
     id = db.Column(db.Integer, primary_key=True)
